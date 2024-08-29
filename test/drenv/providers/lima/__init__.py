cd03c181aab231df91dbd8d4d756c52df8e419d3
Nir Soffer <nsoffer@redhat.com>
2024-08-29 22:32:55 +0300

    Add lima provider for Apple silicon
    We use the "vz" VM type, using Apple virtualization framework, providing
    very good performance. A critical feature is Rosetta 2 integration,
    supporting running amd64 container images if an arm64 image is not
    available. This is required to run OCM since it does not provide arm64
    images for all components.
    
    Lima provisioning is very easy to customize, based on cloud-init and
    yaml configuration, so unlike Minikube, we can easily configure
    everything we need when creating a cluster. However it provisioning is
    very slow since it does not support preloading content like Minikube. We
    can improve this later by creating our own template disk images with
    pre-installed and configured Kubernetes, or using snapshots.
    
    For networking we use shared network via socket_vmnet. This is user
    based network providing host to VM and VM to VM access. Performance is
    poor (1 Gbps, 30 times slower than native networking) but it should be
    fast enough for downloading images.
    
    Another option for Apple silicon is UTM - it uses the same "vz"
    framework but it support fast bridged networking (36 Gbps). However
    Rosetta 2 integration is not complete and it has poor automation and
    provisioning capabilities.
    
    Lima is the default provider for Apple silicon machines.  It can also be
    used on Linux x86_64 machines but it does not have good networking
    solution for Linux, and provisioning is much slower than Minikube.
    
    Lima also support fast snapshots using macOS APFS copy-on-write
    capabilities. This can be extremely useful for fast cleanup during
    development.
    
    The provider configuration is derived from lima-vm k8s.yaml example,
    using only the stable arm64 image and reformatted to fix yamllint
    warnings.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ test/drenv/providers/lima/__init__.py
@@ -0,0 +1,287 @@
+# SPDX-FileCopyrightText: The RamenDR authors
+# SPDX-License-Identifier: Apache-2.0
+
+import importlib.resources as resources
+import json
+import logging
+import os
+import subprocess
+import tempfile
+import time
+
+from drenv import commands
+from drenv import kubeconfig
+from drenv import yaml
+
+LIMACTL = "limactl"
+
+# Important lima statuses
+RUNNING = "Running"
+
+# Options ignored by this provider.
+# TODO: implement what we can.
+UNSUPPORTED_OPTIONS = (
+    "addons",
+    "containerd",
+    "driver",
+    "extra_config",
+    "feature_gates",
+    "network",
+    "service_cluster_ip_range",
+)
+
+# Provider scope
+
+
+def setup():
+    pass
+
+
+def cleanup():
+    pass
+
+
+# Cluster scope
+
+
+def exists(profile):
+    names = _run(LIMACTL, "list", "--format", "{{.Name}}", context=profile["name"])
+    for line in names.splitlines():
+        if line == profile["name"]:
+            return True
+    return False
+
+
+def start(profile, verbose=False):
+    start = time.monotonic()
+    logging.info("[%s] Starting lima cluster", profile["name"])
+
+    if not exists(profile):
+        _log_unsupported_options(profile)
+        with tempfile.NamedTemporaryFile(
+            prefix=f"drenv.lima.{profile['name']}.tmp",
+        ) as tmp:
+            _write_config(profile, tmp.name)
+            _create_cluster(profile, tmp.name)
+
+    _start_cluster(profile)
+    cluster = _get_cluster(profile)
+    _add_kubeconfig(profile, cluster)
+
+    logging.info(
+        "[%s] Cluster started in %.2f seconds",
+        profile["name"],
+        time.monotonic() - start,
+    )
+
+
+def stop(profile):
+    start = time.monotonic()
+    logging.info("[%s] Stopping lima cluster", profile["name"])
+
+    # Stop is not idempotent, and using stop -f does not shutdown the guest
+    # cleanly, resulting in failures on the next start.
+    cluster = _get_cluster(profile)
+    if cluster["status"] == RUNNING:
+        _stop_cluster(profile)
+
+    _remove_kubeconfig(profile)
+
+    logging.info(
+        "[%s] Cluster stopped in %.2f seconds",
+        profile["name"],
+        time.monotonic() - start,
+    )
+
+
+def delete(profile):
+    start = time.monotonic()
+    logging.info("[%s] Deleting lima cluster", profile["name"])
+
+    _delete_cluster(profile)
+    _delete_additional_disks(profile)
+    _remove_kubeconfig(profile)
+
+    logging.info(
+        "[%s] Cluster deleted in %.2f seconds",
+        profile["name"],
+        time.monotonic() - start,
+    )
+
+
+def configure(profile, existing=False):
+    # Cluster is configured when created.
+    pass
+
+
+# Private helpers
+
+
+def _log_unsupported_options(profile):
+    for option in UNSUPPORTED_OPTIONS:
+        if profile[option]:
+            logging.debug(
+                "[%s] Ignoring '%s' for lima cluster",
+                profile["name"],
+                option,
+            )
+
+
+def _write_config(profile, path):
+    """
+    Create vm config for profile at path.
+    """
+    with resources.files("drenv.providers.lima").joinpath("k8s.yaml").open() as f:
+        config = yaml.safe_load(f)
+
+    # The "vz" type is required to support amd64 images on arm64, needed for
+    # OCM, and also provide the best performance.
+    config["vmType"] = "vz"
+    config["rosetta"] = {"enabled": True, "binfmt": True}
+
+    # We always use socket_vmnet to get shared network.
+    config["networks"] = [{"socket": "/var/run/socket_vmnet"}]
+
+    # Add profile options to template
+
+    config["cpus"] = profile["cpus"]
+    config["memory"] = profile["memory"]
+    config["disk"] = profile["disk_size"]
+
+    config["additionalDisks"] = _create_additional_disks(profile)
+
+    with open(path, "w") as f:
+        yaml.dump(config, f)
+
+
+def _create_additional_disks(profile):
+    disks = _list_disks(profile)
+    for disk in disks:
+        logging.info("[%s] Creating disk '%s'", profile["name"], disk["name"])
+        _create_disk(profile, disk)
+    return disks
+
+
+def _delete_additional_disks(profile):
+    for disk in _list_disks(profile):
+        logging.info("[%s] Deleting disk %s", profile["name"], disk["name"])
+        try:
+            _delete_disk(profile, disk)
+        except commands.Error as e:
+            logging.warning(
+                "[%s] Cannot delete disk '%s': %s",
+                profile["name"],
+                disk["name"],
+                e,
+            )
+
+
+def _get_cluster(profile):
+    out = _run(LIMACTL, "list", "--format", "json", context=profile["name"])
+    for line in out.splitlines():
+        cluster = json.loads(line)
+        if cluster["name"] == profile["name"]:
+            return cluster
+    return None
+
+
+def _list_disks(profile):
+    disks = []
+    for i in range(profile["extra_disks"]):
+        disks.append({"name": f"{profile['name']}-disk{i}", "format": False})
+    return disks
+
+
+def _add_kubeconfig(profile, cluster):
+    logging.debug("[%s] Adding lima cluster kubeconfig", profile["name"])
+    src = os.path.join(cluster["dir"], "copied-from-guest", "kubeconfig.yaml")
+    _fixup_kubeconfig(profile, src)
+    kubeconfig.merge(profile, src)
+
+
+def _fixup_kubeconfig(profile, path):
+    with open(path) as f:
+        config = yaml.safe_load(f)
+
+    config["clusters"][0]["name"] = profile["name"]
+    config["users"][0]["name"] = profile["name"]
+
+    item = config["contexts"][0]
+    item["name"] = profile["name"]
+    item["context"]["cluster"] = profile["name"]
+    item["context"]["user"] = profile["name"]
+
+    config["current-context"] = profile["name"]
+
+    with open(path, "w") as f:
+        yaml.dump(config, f)
+
+
+def _remove_kubeconfig(profile):
+    logging.debug("[%s] Removing lima cluster kubeconfig", profile["name"])
+    kubeconfig.remove(profile)
+
+
+def _create_cluster(profile, config):
+    _watch(
+        LIMACTL,
+        "create",
+        "--name",
+        profile["name"],
+        config,
+        context=profile["name"],
+    )
+
+
+def _start_cluster(profile):
+    # Start limactl in a new process group to prevnet killing limactl hostagent on drenv shutdown.
+    # Should be fixed in lima, but this should be good enough for now.
+    _watch(
+        LIMACTL,
+        "start",
+        profile["name"],
+        context=profile["name"],
+        process_group=0,
+    )
+
+
+def _stop_cluster(profile):
+    _watch(LIMACTL, "stop", profile["name"], context=profile["name"])
+
+
+def _delete_cluster(profile):
+    # --force allows deletion of a running cluster.
+    _watch(LIMACTL, "delete", "--force", profile["name"], context=profile["name"])
+
+
+def _create_disk(profile, disk):
+    _watch(
+        LIMACTL,
+        "disk",
+        "create",
+        disk["name"],
+        "--format",
+        "raw",
+        "--size",
+        profile["disk_size"],
+        context=profile["name"],
+    )
+
+
+def _delete_disk(profile, disk):
+    _watch(LIMACTL, "disk", "delete", disk["name"], context=profile["name"])
+
+
+def _run(*args, context="lima"):
+    logging.debug("[%s] Running %s", context, args)
+    return commands.run(*args)
+
+
+def _watch(*args, process_group=None, context="lima"):
+    logging.debug("[%s] Running %s", context, args)
+    for line in commands.watch(
+        *args,
+        stderr=subprocess.STDOUT,
+        process_group=process_group,
+    ):
+        logging.debug("[%s] %s", context, line)
